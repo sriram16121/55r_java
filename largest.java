f508a5647922a8b036526e8a93709a92766ea286
sriram16121 <127281711+sriram16121@users.noreply.github.com>
2025-07-09 16:51:35 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,14 @@
+class largest{
+   public static void main(String[]args){
+        int a=10;	
+	int b=20;
+	int c=30;
+	if(a>b && a>c){
+	System.out.println("the largest number is"+a);}
+	else if(b>c ){
+	System.out.println("the largest number is"+b);}
+	else{
+	System.out.println("the largest number is"+c);}
+		
+}
+}
